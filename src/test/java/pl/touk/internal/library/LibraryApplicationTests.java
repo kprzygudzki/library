476ad3ffa527aab304ab915191153ad2ad8aa7ad
Krzysiek Przygudzki <kpr@touk.pl>
2018-02-03 16:25:33 +0100

    Initial commit

---
+++ src/test/java/pl/touk/internal/library/LibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.touk.internal.library;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
